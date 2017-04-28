74dffedcb1c6d72173e523cdef9eebdfee68eb8d
Gary Houston <ghouston@arglist.com>
2017-04-28 12:40:57 +1000

    adjust for changes in library

--- loop_test.go
+++ loop_test.go
@@ -9,24 +9,25 @@ import (
 // Create a TIFF buffer where one IFD refers to another and vice versa, and
 // check that reading it back gives an error.
 func TestLoop(t *testing.T) {
-	order := binary.LittleEndian
 	var ifd1 IFD_T
+	ifd1.Order = binary.LittleEndian
 	ifd1.Fields = make([]Field, 1)
 	ifd1.Fields[0] = Field{Compression, BYTE, 1, nil}
 	ifd1.Fields[0].Data = []byte("\001")
 	var ifd2 IFD_T
+	ifd2.Order = ifd1.Order
 	ifd2.Fields = make([]Field, 1)
 	ifd2.Fields[0] = ifd1.Fields[0]
 	ifdsize := ifd1.Size() // ifd contains no external data
 	buf := make([]byte, HeaderSize+2*ifdsize)
 	ifd1pos := uint32(HeaderSize)
 	ifd2pos := ifd1pos + ifdsize
-	PutHeader(buf, order, ifd1pos)
-	_, err := ifd1.Put(buf, order, ifd1pos, nil, ifd2pos)
+	PutHeader(buf, ifd1.Order, ifd1pos)
+	_, err := ifd1.Put(buf, ifd1pos, nil, ifd2pos)
 	if err != nil {
 		t.Error("Failed to put ifd1")
 	}
-	_, err = ifd2.Put(buf, order, ifd2pos, nil, ifd1pos)
+	_, err = ifd2.Put(buf, ifd2pos, nil, ifd1pos)
 	if err != nil {
 		t.Error("Failed to put ifd2")
 	}
--- subifds_test.go
+++ subifds_test.go
@@ -9,21 +9,23 @@ import (
 // referencing two other IFDs, and check that it's read back
 // correctly.
 func TestSubIFDs(t *testing.T) {
-	order := binary.LittleEndian
 	var ifd1, ifd2, ifd3 IFD_T
+	ifd1.Order = binary.LittleEndian
 	ifd1.Fields = make([]Field, 1)
 	ifd1.Fields[0] = Field{SubIFDs, IFD, 2, nil}
 	ifd1.Fields[0].Data = make([]byte, 8)
 
+	ifd2.Order = ifd1.Order
 	ifd2.Fields = make([]Field, 1)
 	ifd2.Fields[0] = Field{Compression, SHORT, 1, nil}
 	ifd2.Fields[0].Data = make([]byte, 2)
-	ifd2.Fields[0].PutShort(1, 0, order)
+	ifd2.Fields[0].PutShort(1, 0, ifd2.Order)
 
+	ifd3.Order = ifd1.Order
 	ifd3.Fields = make([]Field, 1)
 	ifd3.Fields[0] = Field{Compression, SHORT, 1, nil}
 	ifd3.Fields[0].Data = make([]byte, 2)
-	ifd3.Fields[0].PutShort(2, 0, order)
+	ifd3.Fields[0].PutShort(2, 0, ifd3.Order)
 
 	node1 := IFDNode{ifd1, TIFFSpace, nil, nil}
 	node2 := IFDNode{ifd2, TIFFSpace, nil, nil}
@@ -35,8 +37,8 @@ func TestSubIFDs(t *testing.T) {
 
 	buf := make([]byte, HeaderSize+node1.TreeSize())
 	ifdpos := uint32(HeaderSize)
-	PutHeader(buf, order, ifdpos)
-	_, err := node1.PutIFDTree(buf, ifdpos, order)
+	PutHeader(buf, ifd1.Order, ifdpos)
+	_, err := node1.PutIFDTree(buf, ifdpos)
 	if err != nil {
 		t.Error(err)
 	}
@@ -44,13 +46,13 @@ func TestSubIFDs(t *testing.T) {
 	if !valid {
 		t.Error("Header not valid")
 	}
-	if getorder != order {
+	if getorder != ifd1.Order {
 		t.Error("Order incorrect")
 	}
 	if getpos != ifdpos {
 		t.Error("Position incorrect")
 	}
-	getnode1, err := GetIFDTree(buf, order, ifdpos, TIFFSpace)
+	getnode1, err := GetIFDTree(buf, getorder, ifdpos, TIFFSpace)
 	if err != nil {
 		t.Error(err)
 	}
@@ -58,19 +60,19 @@ func TestSubIFDs(t *testing.T) {
 		t.Error("Didn't read back 2 sub-IFDs")
 	}
 	getnode2 := getnode1.SubIFDs[0].Node
-	field2 := getnode2.IFD.Fields[0]
+	field2 := getnode2.Fields[0]
 	if field2.Tag != Compression {
 		t.Error("Wrong tag in first sub-IFD.")
 	}
-	if field2.Short(0, order) != 1 {
+	if field2.Short(0, getnode2.Order) != 1 {
 		t.Error("Wrong value in first sub-IFD.")
 	}
 	getnode3 := getnode1.SubIFDs[1].Node
-	field3 := getnode3.IFD.Fields[0]
+	field3 := getnode3.Fields[0]
 	if field3.Tag != Compression {
 		t.Error("Wrong tag in second sub-IFD.")
 	}
-	if field3.Short(0, order) != 2 {
+	if field3.Short(0, getnode3.Order) != 2 {
 		t.Error("Wrong value in second sub-IFD.")
 	}
 }
